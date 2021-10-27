82d2f87f9ff36fdd7320492c756bcb5db9032953
Sydney <sydneyparsonsyb@gmail.com>
2021-10-27 18:43:14 -0400

    comps

--- BenchBnB/BenchBnB/frontend/reducers/root.js
+++ BenchBnB/BenchBnB/frontend/reducers/root.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
+import sessionReducer from './sessions_reducer'
+import usersReducer from "./users_reducer";
 
 const RootReducer = combineReducers({
-
+    session: sessionReducer,
+    user: usersReducer
 })
 
 export default RootReducer;
